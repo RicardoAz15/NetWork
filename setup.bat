59bbdaa2aae46ad682f80af68f52a165040d179f
RicardoAz15 <41963931+RicardoAz15@users.noreply.github.com>
2019-09-12 18:02:25 +0100

    Update Script

--- setup.bat
+++ setup.bat
@@ -1,19 +1,38 @@
 @ECHO OFF
 
-echo Installing Fastlane
-call gem install fastlane -NV
-pause
-echo. 
+echo É necessário instalar/fazer update do Fastlane deseja executar instalação/update
+set /p input= "[y:Intalação u:Update n:Não é necessário] "
+
+if /i "%input%" == "y" (
+	echo Installing Fastlane
+	call gem install fastlane -NV
+	pause
+	echo. 
+)
+
+if /i "%input%" == "u" (
+	echo Updating
+	call fastlane update_fastlane
+	pause
+)
+
+echo.
 echo Install plugins
-cd Retrofit\retrofit_montepio\
-call bundle exec fastlane install_plugins
-cd ..\..\
-echo. 
+call gem install fastlane-plugin-instrumented_tests
+call gem install fastlane-plugin-increment_version_code
+call gem install fastlane-plugin-google_drive
+pause
+
+echo.
+call fastlane install_plugins
+pause
+ 
+echo.
 echo Replacing files
-replace Extra_Files\gradle_helper.rb C:\Ruby26-x64\lib\ruby\gems\2.6.0\gems\fastlane-2.130.0\fastlane\lib\fastlane\helper 
+replace Extra_Files\gradle_helper.rb C:\Ruby26-x64\lib\ruby\gems\2.6.0\gems\fastlane-2.131.0\fastlane\lib\fastlane\helper 
 replace Extra_Files\instrumented_tests_action.rb C:\Ruby26-x64\lib\ruby\gems\2.6.0\gems\fastlane-plugin-instrumented_tests-0.2.0\lib\fastlane\plugin\instrumented_tests\actions
 
-
+echo.
 echo Wich project do u want to test:
 echo 1:Retrofit
 echo 2:Volley
@@ -21,18 +40,26 @@ echo 3:Both
 
 set /p input= "Response: "
 
+echo.
+
+set windowBoll= "true"
+echo Deseja janela no emulador?
+set /p window= "[y/n] "
+
+if /i "%window%" == "y" set windowBoll="false"
+
 if "%input%" == "1" (
 	echo.
 	echo Testing Retrofit
 	cd Retrofit\retrofit_montepio\
-	call bundle exec fastlane test
+	call fastlane test window:%windowBoll%
 	cd ..\..\
 )
 if "%input%" == "2" (
 	echo.
-	echo Testing Retrofit
+	echo Testing Volley
 	cd Volley\Volley_Montepio\
-	call bundle exec fastlane test
+	call fastlane test window:%windowBoll%
 	cd ..\..\
 )
 if "%input%" == "3" (
@@ -41,18 +68,17 @@ if "%input%" == "3" (
 	echo.
 	echo Testing Retrofit
 	cd Retrofit\retrofit_montepio\
-	call bundle exec fastlane test
+	call fastlane test window:%windowBoll%
 	cd ..\..\
 	echo.
 	PAUSE
 	echo.
 	echo Testing Retrofit
 	cd Volley\Volley_Montepio\
-	call bundle exec fastlane test
+	call fastlane test window:%windowBoll%
 	cd ..\..\
 )
 
 PAUSE
 
-start iexplore.exe https://drive.google.com/drive/folders/1w6Q1lr42Bx3-AMxU5pNd_MkBACMOFtUv?usp=sharing 
-start iexplore.exe https://ricardo-set3240.slack.com/messages/CMS8RA4BY
+start "link" https://drive.google.com/drive/folders/1w6Q1lr42Bx3-AMxU5pNd_MkBACMOFtUv?usp=sharing && start "link" https://ricardo-set3240.slack.com/messages/CMS8RA4BY
